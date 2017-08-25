c284a09990e3aee6e77081b83755f26735a552bc
uproad <uproad3@gmail.com>
2017-08-25 11:21:26 +0900

    [change] 不要な入力要求をしているパラメーターチェックを削除

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -69,6 +69,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :username, :body, :parent_id, :like, :bad)
+      params.require(:article).permit(:title, :username, :body)
     end
 end
