a34ca128f3e92aa9526d1b2b297a5d51b91d6af1
uproad <uproad3@gmail.com>
2017-08-25 12:04:21 +0900

    [create] like-bad機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :articles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles do
+    member do
+      post :like
+      post :bad
+    end
+  end
 end
